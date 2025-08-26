9037d4b3815fdd32aa6c4a2859a56ead7932b589
¨ayatahmedelsayedmohammed.baghoush¨ <ayatahmedelsayedmohammed.baghoush@vodafone.com>
2025-08-26 21:03:51 +0200

    Adding the feature of cross-browser compatibility testing

--- playwright.config.js
+++ playwright.config.js
@@ -2,5 +2,19 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   reporter: "allure-playwright",
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'WebKit',
+      use: { browserName: 'webkit' },
+    },
+  ],
 });
 
