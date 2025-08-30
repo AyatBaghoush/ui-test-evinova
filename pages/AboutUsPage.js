ccffa9474ac06233d581705bc5b71f73f378a098
¨ayatahmedelsayedmohammed.baghoush¨ <ayatahmedelsayedmohammed.baghoush@vodafone.com>
2025-08-30 03:51:22 +0200

    Updating aboutUs page to have 2 methods to check the visibility of team profiles & company timeline

--- pages/AboutUsPage.js
+++ pages/AboutUsPage.js
@@ -3,8 +3,19 @@ const BasePage = require('./BasePage');
 class AboutUsPage extends BasePage {
   constructor(page) {
     super(page);
+    this.historyTimeline = '.container-global-timeline';
+    this.teamMemberSection ='.our-team_wrapper'
   }
 
+  async isHistoryTimelineVisible() {
+    await this.page.locator(this.historyTimeline).scrollIntoViewIfNeeded();
+    return this.page.isVisible(this.historyTimeline);
+  }
+
+    async areTeamMemberProfilesVisible() {
+    await this.page.locator(this.teamMemberSection).scrollIntoViewIfNeeded();
+    return this.page.isVisible(this.teamMemberSection);
+  }
 
 }
 
